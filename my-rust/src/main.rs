d3b5f97e567f71c13097bb1c897ec064185356a2
zhujinxiong <zhujinxiong@patsnap.com>
2019-08-06 23:35:26 +0800

    Define struct again

--- my-rust/src/main.rs
+++ my-rust/src/main.rs
@@ -1,8 +1,16 @@
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
+    age: u64,
     active: bool,
+}
+
+impl User {
+    fn area(&self) -> u64 {
+        self.age * self.sign_in_count
+    }
 }
 
 fn main() {
@@ -11,7 +19,9 @@ fn main() {
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
+        age: 2,
     };
 
-    println!("Hello, world! {}", user1.email);
+    println!("Hello, world! {:?}", user1);
+    println!("result: {}", user1.area());
 }

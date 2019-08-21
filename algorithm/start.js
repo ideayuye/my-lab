bfcc751dd87dbfae627e6e7a9da82cd9ba89788b
zhujinxiong <zhujinxiong@patsnap.com>
2019-08-21 22:47:26 +0800

    Add fibonaqi

--- algorithm/start.js
+++ algorithm/start.js
@@ -1,6 +1,28 @@
+/**
+ * 有N个台阶，如果一次可以走一步，也可以走两步，有几种走法
+ *  */
 var step = (n) => {
-    var s1 = n - 1;
-    var s2 = n - 2;
-    var x1, x2;
-    n = x1 + 2*x2;
+    var x1 = 1;
+    var x2 = 1;
+    var arr = [x1, x2];
+    var i = 2;
+    while(i <= n) {
+        var temp = arr[1];
+        arr[1] = arr[0] + arr[1];
+        arr[0] = temp;
+        i++;
+    }
+    return arr[1];
 }
+
+// 递归
+var Max = (i) => {
+    if (i <= 2) {
+        return i;
+    }
+    return Max(i - 1) + Max(i - 2);
+}
+
+var num = 100;
+// console.log(Max(num));
+console.log(step(num));

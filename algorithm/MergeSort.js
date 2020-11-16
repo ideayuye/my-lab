6014e03791045c98f1070259ed4caacff475e40c
zhujinxiong <zhujinxiong@patsnap.com>
2020-11-16 22:03:03 +0800

    Modify merge sort

--- algorithm/MergeSort.js
+++ algorithm/MergeSort.js
@@ -8,39 +8,19 @@ class MergeSort {
         if (start >= end) {
             return;
         }
-        // const mid = start + Math.floor((end - start)/2);
-        const mid = Math.floor((end + start)/2);
+        const mid = start + Math.floor((end - start)/2);
         this.mergeSort(arr, start, mid);
         this.mergeSort(arr, mid + 1, end);
-        arr = this.merge(arr, start, mid, end);
+        this.merge(arr, start, mid, end);
         return arr;
     }
 
     merge(arr, start, mid, end) {
-        let left = arr.slice(start, mid);
-        let right = arr.slice(mid, end);
-        console.log(start, mid, end);
-        // console.log('left', left, 'right', right);
-        // for(let i = start, l=0, r=0; i < (end - start + 1); i++) {
-        //     if (left.length === 0) {
-        //         arr[i] = right[r];
-        //         r++;
-        //         continue;
-        //     }
-        //     if (right.length === 0) {
-        //         arr[i] = left[l];
-        //         l++;
-        //         continue;
-        //     }
-        //     if (left[l] < right[r]) {
-        //         arr[i] = left[l];
-        //         l++;
-        //     } else {
-        //         arr[i] = right[r];
-        //         r++;
-        //     }
-        // }
-        let i = -1;
+        let left = arr.slice(start, mid + 1);
+        let right = arr.slice(mid + 1, end + 1);
+        // console.log(start, end);
+        console.log('left', left, 'right', right);
+        let i = start - 1;
         while(left.length || right.length) {
             i++;
             if (left.length && !right.length) {
@@ -57,9 +37,8 @@ class MergeSort {
                 arr[i] = right.shift();
             }
         }
-        
-        // arrRes = arrRes.concat(left, right);
-        console.log('merged', arr);
+
+        // console.log('merged', arr);
         return arr;
     }
 }
@@ -67,11 +46,11 @@ class MergeSort {
 const m = new MergeSort();
 
 const arr = [3, 6, 9, 2, 3, 5, 34];
-// const t1 = ArrayGenerator(8);
-const t1 = [62, 15, 74, 87, 19, 36, 48, 76];
+const t1 = ArrayGenerator(9);
+// const t1 = [62, 15, 74, 87, 19, 36, 48, 76];
 
 // console.log(m.merge(t1, 0, 3, ));
 console.log(t1.slice(0));
-console.log('result', m.mergeSort(t1, 0, t1.length));
+console.log('result', m.mergeSort(t1, 0, t1.length - 1));
 
 console.log('hold on');

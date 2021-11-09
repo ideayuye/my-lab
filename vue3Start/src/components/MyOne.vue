296477b658537590b5ce359d354d6022a3b348ce
zhujinxiong <zhujinxiong@patsnap.com>
2021-11-09 19:58:36 +0800

    Modify myone

--- vue3Start/src/components/MyOne.vue
+++ vue3Start/src/components/MyOne.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>count:{{readersNumber}}</div>
+    <div>
+        <div>count:{{readersNumber}}</div>
+        <button @click="test">test</button>
+        <ul>
+            <li v-for="(item, index) in arrReactive" :key="index">{{item.a}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -9,13 +15,21 @@ export default {
     name: 'MyOne',
     setup() {
         const readersNumber = ref(0);
+        const arr = [{a:1}, {a:2}, {a: 3}];
+        const arrReactive = reactive(arr);
 
         return {
-            readersNumber
+            readersNumber,
+            arrReactive
         };
     },
     mounted() {
         this.readersNumber ++;
+    },
+    methods: {
+        test() {
+            this.arrReactive[0].a = 'xfa';
+        }
     }
 }
 </script>
